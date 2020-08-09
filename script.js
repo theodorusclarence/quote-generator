772e3a80e9933f6634f43737a63d3d9440dcb782
Theodorus Clarence <Clarence@Clarences-MacBook-Air.local>
2020-08-09 21:17:42 +0700

    configure newQuote, twitterBtn, sizing on longer texts

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 // Get quotes from API
 async function getQuote() {
     //proxy to prevent CORS error
@@ -6,12 +12,40 @@ async function getQuote() {
     try {
         response = await fetch(proxyUrl + encodeURIComponent(apiUrl));
         data = await response.json();
-        console.log(data);
+
+        //get the contents because of proxy API
+        dataJSON = JSON.parse(data.contents);
+
+        //if the author name is unavailable
+        if (dataJSON.quoteAuthor === "") {
+            quoteAuthor.innerText = "Unknown";
+        } else {
+            quoteAuthor.innerText = dataJSON.quoteAuthor;
+        }
+
+        //Reduce size for longer text
+        if (dataJSON.quoteText.length > 50) {
+            quoteText.classList.add("long-quote");
+        } else {
+            quoteText.classList.remove("long-quote");
+        }
+        quoteText.innerText = dataJSON.quoteText;
     } catch (e) {
         getQuote();
         console.log("noquote", e);
     }
 }
+
+const tweetQuote = () => {
+    const quote = dataJSON.quoteText;
+    const author = dataJSON.quoteAuthor;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, "_blank");
+};
+
+//Event listener
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // Onload
 getQuote();
